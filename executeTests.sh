326f5fae41ab47565e8e64855e7615455e20e4bd
Bruno Domingues <bruno_signorelli@outlook.com>
2021-04-07 15:06:02 -0300

    Added arg or user input instead of hardcoded path

--- executeTests.sh
+++ executeTests.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' 'Traceback()' '49' '8' 'Traceback()' '5' 'Traceback()')
+R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' '49' '-2' '-6' '1' '2' '1' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()' 'Traceback()')
 files=(./tests/*)
 N_tests=${#R[@]}
 
-for (( i=0; i<$N_tests; i++));
-do
-    A=$(python3 ../main.py ${files[$i]} 2>&1)
-    if ! [[ $A == ${R[$i]} ]]; then # if not equal to number in list
-        if ! [[ $A =~ ^${R[$i]}  ]]; then # if regex doesn't match
-            echo "Failure on ${files[$i]}. Expected ${R[$i]}, but got $A." 
-            exit; 
+FILE=""
+
+if [ $# -eq 0 ]
+  then
+    read -p "Enter file path: " FILE
+  else
+    FILE="$1"
+fi
+
+for ((i = 0; i < $N_tests; i++)); do
+    A=$(python3 $FILE ${files[$i]} 2>&1)
+    if ! [[ $A == ${R[$i]} ]]; then      # if not equal to number in list
+        if ! [[ $A =~ ^${R[$i]} ]]; then # if regex doesn't match
+            echo "Failure on ${files[$i]}. Expected ${R[$i]}, but got $A."
+            exit
         fi
     fi
 done
