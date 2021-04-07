31cb753ed392aac8cfb7b02b07ec4f0eb076c13f
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-04-07 08:25:47 -0300

    mais testes

--- executeTests.sh
+++ executeTests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' 'Traceback()' '49')
+R=('2' '1' '77' '3' '3' '4' '1' '145' '-1' '5' 'Traceback()' '49' '8' 'Traceback()' '5' 'Traceback()')
 files=(./tests/*)
 N_tests=${#R[@]}
 
 for (( i=0; i<$N_tests; i++));
 do
-    A=$(python3 main.py ${files[$i]} 2>&1)
+    A=$(python3 ../main.py ${files[$i]} 2>&1)
     if ! [[ $A == ${R[$i]} ]]; then # if not equal to number in list
         if ! [[ $A =~ ^${R[$i]}  ]]; then # if regex doesn't match
             echo "Failure on ${files[$i]}. Expected ${R[$i]}, but got $A." 
